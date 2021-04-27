27bf58703b50e861e2d15287f1a22924b4d10ead
Jared Kosanovic <jared.kosanovic@wisc.edu>
2021-04-27 14:52:17 -0500

    Parameterize region, project

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,4 @@
 provider "google" {
-  project = "doit-ipt-apigee-dev-808d"
-  region  = "us-central1"
-  # TODO: Do we need to specify a zone?
-  # zone    = "us-central1-c"
+  project = var.gcp_project
+  region  = var.gcp_region
 }
