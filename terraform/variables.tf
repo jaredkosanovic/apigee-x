152cc7a0e9c7eafa0bb40bfaf5d6a7e9ed1d3acb
Jared Kosanovic <jared.kosanovic@wisc.edu>
2021-04-27 15:27:44 -0500

    Fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "gcp_region" {
   type        = string
   description = "GCP region"
-  default = "us-central1"
+  default     = "us-central1"
 }
 
 variable "gcp_project" {
   type        = string
   description = "GCP project name"
-  default = "doit-ipt-apigee-dev-808d"
+  default     = "doit-ipt-apigee-dev-808d"
 }
